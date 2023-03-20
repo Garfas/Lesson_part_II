fe566f478f7897bca6a6c8b079ff887294078331
Mantas <62988421+Garfas@users.noreply.github.com>
2023-03-20 18:10:04 +0200

    .

--- clean_code_II.py
+++ clean_code_II.py
@@ -1,1 +1,53 @@
-a = [1, 1,    1,   1]
+# 2023_03_16
+
+
+class Vehile:
+    def __init__(self, name: str, seatings_capacity: str, max_speed: str):
+        self.name = name
+        self.seatings_capacity = seatings_capacity
+        self.max_speed = max_speed
+
+    def star(self):
+        print(f"{self.name} has started,")
+
+    def stop(self):
+        print(f"{self.name} has stoped,")
+
+    def accelerate(self):
+        print(f"{self.name} is accelarating,")
+
+    def brake(self):
+        print(f"{self.name} is braking.")
+
+    def fare_charge(self):
+        fare = self.seatings_capacity * 100
+        if isinstance(self, Bus):
+            fare *= 1.1
+        return fare
+
+
+class Bus(Vehile):
+    def __init__(self, name: str, seatings_capacity: str, max_speed: str):
+        super().__init__(name, seatings_capacity, max_speed)
+
+
+class Taxi(Vehile):
+    def __init__(self, name: str, seatings_capacity: str, max_speed: str):
+        super().__init__(name, seatings_capacity, max_speed)
+
+    def hail(self):
+        print(f"{self.name} has been hailed.")
+
+    def pay_fare(self):
+        print(f"{self.name} has been paid the fare.")
+
+
+class Train(Vehile):
+    def __init__(
+        self, name: str, seatings_capacity: str, max_speed: str, num_cars: str
+    ):
+        super().__init__(name, seatings_capacity, max_speed)
+        self.num_cars = num_cars
+
+    def announce_stop(self):
+        print(f"{self.name}")
