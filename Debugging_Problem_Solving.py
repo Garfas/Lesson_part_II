c8c75ae0390a08986b1f4ef7022713507104245e
Mantas <62988421+Garfas@users.noreply.github.com>
2023-03-22 20:11:13 +0200

    .

---
+++ Debugging_Problem_Solving.py
@@ -0,0 +1,16 @@
+# 2023_03_21
+
+import sys
+
+total = 0
+
+for arg in sys.argv[1:]:
+    try:
+        num = float(arg)
+        total += num
+
+    except ValueError:
+        print("Error: invalid argument '{}'".format(arg))
+        sys.exit(1)
+
+print("Total: {}".format(total))
