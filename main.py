20ae19553fd63c2e355665615134d2cd00be8107
Mantas <62988421+Garfas@users.noreply.github.com>
2023-04-03 18:42:40 +0300

    .

--- main.py
+++ main.py
@@ -14,40 +14,132 @@
 
 
 
-"""Ask the user if he has reserved a table, and if not, assign him a table based on availability. 
-There should be a certain number of tables, 1 for single, double and family size."""
+import datetime
+import logging
 
+class table:
+    def _init (self, number, seats):
+        self.number = number
+        self.seats = seats
+        self.reserved = False
+        self.reservation = None
+   
+        
+class Reservation:
+    def __init__(self, name, phone, num_guests, time, date):
+        self.name = name
+        self.phone = phone
+        self.num_guests = num_guests
+        self.time = time
+        self.date =date
+        
 
+class Menu:
+    def __init__(self) -> None:
+        self.menu:dict= {}
+           
+    def add_breakfast_item(self, name, weight, preparation_time, calories, price, vegetarian=False):
+        if "breakfast" not in self.menu:
+            self.menu["breakfast"] = [
+                {"name": "Eggs Benedict", "weight": 300, "prep_time": 15, "calories": 500, "price": 20},
+                {"name": "Pancakes", "weight": 200, "prep_time": 10, "calories": 400, "price": 15}  
+            
+            ]
+        self.menu["breakfast"].append({"name": name, "weight": weight, "prep_time": preparation_time, "calories": calories, "price": price, "vegetarian": vegetarian})
 
+    def add_lunch_item(self, name, weight, preparation_time, calories, price, vegetarian=False):
+        if "lunch" not in self.menu:
+            self.menu["lunch"] = [
+                {"name": "Caesar Salad", "weight": 250, "prep_time": 5, "calories": 300, "price": 12},
+                {"name": "Burger", "weight": 350, "prep_time": 15, "calories": 800, "price": 18}  
+            
+            ]
+        self.menu["lunch"].append({"name": name, "weight": weight, "prep_time": preparation_time, "calories": calories, "price": price, "vegetarian": vegetarian})
 
-"""After assigning a table, show the number of free and reserved/occupied tables. 
-For a reserved table, you can specify the name and time of reservation."""
+    def add_dinner_item(self, name, weight, preparation_time, calories, price, vegetarian=False):
+        if "dinner" not in self.menu:
+            self.menu["dinner"] = [
+                {"name": "Grilled Salmon", "weight": 300, "prep_time": 20, "calories": 450, "price": 25},
+                {"name": "Steak", "weight": 400, "prep_time": 25, "calories": 1000, "price": 30},
+                
+            ]
+        self.menu["dinner"].append({"name": name, "weight": weight, "prep_time": preparation_time, "calories": calories, "price": price, "vegetarian": vegetarian})
+    
+    def add_drink_item(self, name, weight, preparation_time, calories, price, vegetarian=False):
+        if "drinks" not in self.menu:
+            self.menu["drinks"] = [
+                {"name": "Coca cola", "weight": 250, "prep_time": 2, "calories": 250, "price": 2},
+                {"name": "Water", "weight": 250, "prep_time": 2, "calories": 0, "price": 1},
+                
+            ]
+        self.menu["drinks"].append({"name": name, "weight": weight, "prep_time": preparation_time, "calories": calories, "price": price, "vegetarian": vegetarian})
+        
+        
+class Payment:
+    def __init__(self):
+        self.reservation = Reservation
+        
+    def select_payment_method(self):
+        if self.reservation.table.reserved:
+            print("Please select a payment method:")
+            print("1", "Cash")
+            print("2", "Bank card")
+            choice = input("Enter your payment method (Cash, Bank)")
+            if choice == "Cash":
+                print("Payment received in cash.")
+                logging.info("Payment received in cash:{}".format(self.reservation.table.reserved))
+            elif choice == "Bank card":
+                print("Payment received by bank card.")
+                logging.info("Payment received by bank card:{}".format(self.reservation.table.reserved))
+            self.reservation.table.reserved = False
+            self.reservation.table.reservation = None
+            print("Reservation cancelled.")
+            logging.warning("Reservation canceled after payment::{}".format(self.reservation.table))
+        else:
+            print("No reservation found.")
+            logging.info("No reservation found for payment::{}".format(self.reservation.table))
+            
+logging.info(filename='restaurant.log', level=logging.INFO)
 
-
-
-
-""""Then show different menu options for breakfast, lunch, dinner and drinks. 
-Include a special menu for vegetarians and vegans. Each menu item must have the following attributes: 
-weight, preparation time in minutes, calories and price."""
-
-
-
-
-"""Allow the user to change the order before payment. This means they should be able to add, 
-delete or update the amount of the same order as per their choice."""
-
-
-
-
-"""Show the final order summary to the user, including the menu items selected, the total amount due,
- and the approximate wait time for the food to be served."""
-
-
-
-
-"""Add an option for the user to add a tip as a percentage of the total price to the final bill. 
-And ask whether the payment will be in cash or by credit card."""
-
-
-
-"""After making the payment, generate a receipt by registering the transaction data."""
+class Restaurant:
+    def __init__(self) -> None:
+        
+            
+            
+            
+            
+# def main():
+#     restaurant = Restaurant()
+#     while True:
+#         print ("Welcome to the restaurant")
+#         print("Please select an optinion")
+#         print("1. View tables")
+#         print("2. Make a new reservation")
+#         print("3. Cencel a reservation")
+#         print("4. Make a peyment")
+#         print("5. View menu")
+#         print("0. Exit")
+        
+#         choices = input("Enter your choice: ")
+        
+#         if choices == "1":
+#             restaurant.view_table()
+#         elif choices == "2":
+#             reservation = reservation.view_table()
+#             restaurant.make_reservation(reservation)
+#         elif choices == "3":
+#             table_number = input("Enter the table number to cancel the reservation ")
+#             restaurant.cencel_reservation(int(table_number))
+#         elif choices == "4":
+#             table_number = input("Enter the table number to make the payment ")
+#             restaurant.make_payment(int(table_number))
+#         elif choices == "5":
+#             restaurant.view_menu()
+#         elif choices == "0":
+#             break
+#         else:
+#             print("Invalid choice. Pleas try again")
+            
+# if __name__ == "__main__":
+#     main()
+            
---
+++ oop_advanced5.py
@@ -0,0 +1,99 @@
+# 2023.03.30
+# OOP Advanced [lesson5]: Class instance, static methods.
+
+# Task Nr.1
+# Create a class which takes temperature measurements in Kelvins and add static methods to\
+    # transform those to Celsius and Fahrenheit units. Use OOP abstraction.
+
+
+# from abc import ABC, abstractclassmethod
+
+# class TemperatureConverter (ABC):
+#     @staticmethod
+ 
+#     def kelvin_to_Celsius(kelvin):
+#         pass
+    
+#     @staticmethod
+  
+#     def kelvin_to_fahrenheit(kelvin):
+#         pass
+    
+# Class Temperature(TemperatureConverter):
+    
+#     def __init__(self, kelvin):
+#         self.kelvin = kelvin
+        
+#     def get_kelvin(self):
+#         return self.kelvin
+    
+#     def set_kelvin(self, kelvin ):
+#         self.kelvin = kelvin    
+        
+#     @staticmethod
+#     def kelvin_to_Celsius(kelvin):
+#         return kelvin - 273.15
+    
+#     @staticmethod
+#     def kelvin_to_fahrenheit(kelvin):
+#         return kelvin * 9/5 - 459.67
+    
+#     def __str__(self):
+#         return f'{self.kelvin} K'
+    
+# temp = Temperature(100)
+# pritn(f'{temp.get_kelvin()} K is equal to {Temperature.kelvin_to_celsius(temp.get_kelvin())} C')
+# pritn(f'{temp.get_kelvin()} K is equal to {Temperature.kelvin_to_fahrenheit(temp.get_kelvin())} F')
+
+# Task Nr.2:
+# Create a class that would take at least five imperial system measurements and would transform with the help \
+    # of static methods to metric system units.
+
+# class ImperialToMetric:
+#     @staticmethod
+#     def inch_to_cm(inch):
+#         return inch * 2.5
+    
+#     @staticmethod
+#     def foot_to_mm(foot):
+#         return foot * 0.30
+    
+#     @staticmethod
+#     def yard_to_m(yard):
+#         return yard * 0.91
+        
+#     @staticmethod
+#     def mile_to_km(mile):
+#         return mile * 1.6
+    
+#     @staticmethod
+#     def pound_to_kg(pound):
+#         return pound * 0.45
+    
+# imperial = ImperialToMetric()
+# print(imperial.inch_to_cm(10)) #25.0
+# print(imperial.foot_to_mm(5)) #1.5
+# print(imperial.yard_to_m(2)) #1.82
+# print(imperial.mile_to_km(10)) #16.0
+# print(imperial.pound_to_kg(100)) #45.0
+
+
+
+
+    
+    
+# 2023 04 03
+
+# class Calc:
+#     MY_NUMBER = 2
+#     def __init__(self, nr: int ):
+#         self.MY_NUMBER+= nr
+        
+# calc_one = Calc(nr=3)
+# print(calc_one.MY_NUMBER)
+
+# cal_two = Calc(nr=5)
+# print(cal_two.MY_NUMBER)
+
+
+
